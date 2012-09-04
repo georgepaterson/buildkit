7978cb198029a07844b7b28426f636e2e0ee0b77
George Paterson <paterson.george@googlemail.com>
2012-09-04 09:07:48 +0100

    Commenting the project JavaScript control file.

--- www/script/example.js
+++ www/script/example.js
@@ -1,3 +1,12 @@
+/*!
+	Example project control file.
+	JavaScript feature should be instatiated through this file.		
+*/s
+/*
+	Configuration paths for requirejs.
+	These allow short reference paths later in the code that can be maintain in a single area.
+	A default feature of requirejs.		
+*/
 require.config({
     baseUrl: '/script',
     paths: {
@@ -5,14 +14,29 @@ require.config({
         'modules': '/script/modules'
     }
 });
+/*
+	Test if jQuery is available, if not load it.
+	Features in the following block will require jQuery.		
+*/
 require(['jquery'], function($) {
+	/*
+		Dialog references.
+		Initially test to see if the dialog module will be required on the page.
+		If so using requirejs to test if the module is available, if not load it. 
+	*/
 	if ($('.dialog-link, .dialog-remote, .dialog-iframe, .dialog-fragment').length) {
 		require(['jquery', 'modules/dialog'], function($) {
+			/*
+				Dialog content loaded from an inline document object. 
+			*/
 			$('.dialog-link').on('click', function(event){
 				var link = $(this).attr('href');
 				event.preventDefault();				
 				$(link).dialog({modal: true});
 			});
+			/*
+				Remote dialog content using the jQuery load method. 
+			*/
 			$('.dialog-remote').on('click', function(event){
 				var link = $(this).attr('href'),
 					hash = link.replace(/.*(?=#[^\s]+$)/, ''),
@@ -24,6 +48,9 @@ require(['jquery'], function($) {
 					$(this).find(hash).dialog();
 				});
 			});
+			/*
+				Dialog content loaded through an iFrame. 
+			*/
 			$('.dialog-iframe').on('click', function(event){
 				var link = $(this).attr('href'),
 					fragment = $('<div><header><a class="dialog-close" href="#" role="button">Close dialog</a></header></div>');
